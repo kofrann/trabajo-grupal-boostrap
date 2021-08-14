dc50cd5b06d0af6fdce13d80d5db79d17bc5cb12
Franco Urra <kofran.cl@gmail.com>
2021-08-14 11:16:11 -0400

    cambio 3
    se agregan validaciones js en el formulario

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 var d = new Date();
 document.getElementById("fecha").innerHTML = d;
+
+
+
+function validar() {
+    var nombre =document.getElementById("nombres").value;
+        if (nombre === '') {
+        alert('Ingrese Nombre');
+    }
+    var Email = document.getElementById("Email1").value;
+    if (Email === '') {
+        alert('Ingrese E-mail');
+    }
+    var Password = document.getElementById("Password1").value;
+    if (Password === '') {
+        alert('Ingrese Password')
+    }
+ }
