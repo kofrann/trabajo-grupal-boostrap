d8c4b44f59f43f62b72fcede710a450a56a69de8
Franco Urra <kofran.cl@gmail.com>
2021-08-17 20:37:02 -0400

    cambio 4
    Se agrega el plugin jquery validation , para validacion del form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 var d = new Date();
 document.getElementById("fecha").innerHTML = d;
 
-
-
+/*
 function validar() {
     var nombre =document.getElementById("nombres").value;
         if (nombre === '') {
@@ -17,3 +16,45 @@ function validar() {
         alert('Ingrese Password')
     }
  }
+ */
+
+$(document).ready(function () {
+  $("#form1").validate({
+    rules: {
+        nombres: {
+        required: true,
+        minlength: 3,
+        maxlength: 20,
+      },
+      apellido: {
+        required: true,
+        minlength: 3,
+        maxlength: 20,
+      },
+      Email1: {
+        required: true,
+        minlength: 10,
+        maxlength: 30,
+      },
+    },
+
+    messages:{
+      nombres: {
+        required: "Debe Ingresar el Nombre",
+        minlength: "El nombre debe contener al menos 3 caracteres",
+        maxlength: "El nombre debe contener al menos 20 caracteres",
+      },
+      apellido: {
+        required: "Debe Ingresar el Apellido",
+        minlength: "El nombre debe contener al menos 3 caracteres",
+        maxlength: "El nombre debe contener al menos 20 caracteres",
+      },
+      Email1: {
+        required: "Debe Ingresar el Correo",
+        minlength: "El nombre debe contener al menos 3 caracteres",
+        maxlength: "El nombre debe contener al menos 20 caracteres",
+      },
+    },
+  });
+});
+
